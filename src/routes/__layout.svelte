8bf8384c0d79dd8b1b632644dd5d21b8fb084cae
Ole Schley <schley.ole@gmail.com>
2022-05-07 21:46:29 +0200

    navbar and basic layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,19 @@
 <script>
   import "../app.css";
+
+  import Navbar from "$lib/Navbar.svelte"
+
 </script>
 
-<slot />
+<div class="flex flex-row">
+
+  <div class="basis-40">
+    <Navbar></Navbar>
+  </div>
+  
+
+  <div class="basis-96 grow">
+    <slot/>
+  </div>
+
+</div>
---
+++ src/routes/viz/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    import ChartCard from "$lib/ChartCard.svelte";
+    import Population from "./charts/population.svelte";
+</script>
+
+<div class=" flex grid grid-cols-2 gap-2">
+
+    <ChartCard title="Population of random cities">
+        <Population></Population>
+    </ChartCard>
+
+</div>
